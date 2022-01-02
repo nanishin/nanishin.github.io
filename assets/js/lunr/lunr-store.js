2af58ceed2d7419d562dca8c239f8dedd7b1ab06
Nani Shin <nani.shin@gmail.com>
2022-01-03 02:43:12 +0900

    Update post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -388,4 +388,9 @@ var store = [{
         "excerpt":"21세기 들어 기후변화, 우주로의 진출 현실화를 꿈꾸며 로켓처럼 가속화되고 있는 패러다임 변화 속에서 역으로 뉴트로에 대한 관심은 커져가는데 이번 옷소매 붉은 끝동에서 과함을 덜어내고 이야기를 풀어가는 담백함에 영감을 받아 어릴 때부터 들어온 고령 신씨 후손이기에 풀어낼 수 있는 이야기를 하나씩 하나씩 남겨보기로 함 역사적인 쟁점도 다양하고 판타지적이며 SF적인 상황들이 언급되겠지만...","categories": ["history"],
         "tags": ["고령 신씨","Goryeong Shin clan","역사","history","이야기","story"],
         "url": "https://nanishin.github.io/history/history-start-of-story/",
-        "teaser":"https://nanishin.github.io/assets/images/nanishin.jpg"}]
+        "teaser":"https://nanishin.github.io/assets/images/nanishin.jpg"},{
+        "title": "문화충격을 준 신숙주 할아버지와 청의동자",
+        "excerpt":"일제 강점기 기간(1910년 8월 29일 ~ 1945년 8월 15일) 춘원 이광수가 공개적인 친일로 변절(1922년 월간 잡지 개벽 5월호에 민족개조론 발표)한 후 동아일보에서 1년 가량 연재(1928년 11월 30일 ~ 1929년 12월 1일)했던 단종애사… 사건의 역사적 시간 순서를 바꿔가면서까지 신숙주 할아버지에 대한 배신/변절 표현을 격렬하게 묘사한 것을 어린 시절 접하고 충격에 휩싸였는데...","categories": ["history"],
+        "tags": ["신숙주","Sukju Shin","ShinSukJu","청의동자","Cheonguidongja","Cheong-ui-dong-ja","역사","history","이야기","story","자비스","jarvis"],
+        "url": "https://nanishin.github.io/history/history-culture-shock-by-cheonguidongja/",
+        "teaser":"https://nanishin.github.io/assets/images/shinmunsegeo_cheonguidongja.jpg"}]
